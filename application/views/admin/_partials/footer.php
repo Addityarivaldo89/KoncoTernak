bb94c3809489f6aeceb9fcf844be5b5e6bb0bcbe
Additya Rivaldo <addityarivaldo89@gmail.com>
2020-02-01 00:33:47 +0700

    upload baru

---
+++ application/views/admin/_partials/footer.php
@@ -0,0 +1,8 @@
+<!-- Sticky Footer -->
+<footer class="sticky-footer">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>&copy Copyright KoncoTernak <?php echo Date('Y') ?></span>
+    </div>
+  </div>
+</footer>
---
+++ application/views/admin/_partials/js.php
@@ -0,0 +1,15 @@
+<!-- Bootstrap core JavaScript-->
+<script src="<?php echo base_url('assets/res/jquery/jquery.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/res/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="<?php echo base_url('assets/res/jquery-easing/jquery.easing.min.js') ?>"></script>
+<!-- Page level plugin JavaScript-->
+<script src="<?php echo base_url('assets/res/chart.js/Chart.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/res/datatables/jquery.dataTables.js') ?>"></script>
+<script src="<?php echo base_url('assets/res/datatables/dataTables.bootstrap4.js') ?>"></script>
+<!-- Custom scripts for all pages-->
+<script src="<?php echo base_url('assets/resjs/sb-admin.min.js') ?>"></script>
+<!-- Demo scripts for this page-->
+<script src="<?php echo base_url('assets/resjs/demo/datatables-demo.js') ?>"></script>
+<script src="<?php echo base_url('assets/resjs/demo/chart-area-demo.js') ?>"></script>
